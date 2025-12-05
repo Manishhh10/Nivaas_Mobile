ce1b33abba0d6c6dd78b83610b67f8dc53c62c09
Manishhh10 <manishbhatt2121714@gmail.com>
2025-12-05 14:12:23 +0545

    splash screen added in splash-screen branch

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,27 @@
+// lib/screens/splash_screen.dart
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/logo.png'),
+          ],
+        ),
+      ),
+    );
+  }
+}
