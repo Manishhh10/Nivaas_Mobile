dc50c6e46f135ab0afd194786274f5c646340bd3
Manishhh10 <manishbhatt2121714@gmail.com>
2025-12-19 13:02:59 +0545

    trips screen created

---
+++ lib/screens/bottom screen/trips_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class TripsScreen extends StatelessWidget {
+  const TripsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Trips", style: Theme.of(context).textTheme.titleLarge),
+    );
+  }
+}
