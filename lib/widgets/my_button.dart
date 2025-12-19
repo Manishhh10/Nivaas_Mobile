3c96e840f36f5b8e702e7aa0c653d50cfc5a9dca
Manishhh10 <manishbhatt2121714@gmail.com>
2025-12-19 16:17:56 +0545

    my_button updated to use thme_data

--- lib/widgets/my_button.dart
+++ lib/widgets/my_button.dart
@@ -3,15 +3,11 @@ import 'package:flutter/material.dart';
 class MyButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
-  final Color? backgroundColor;
-  final Color? textColor;
 
   const MyButton({
     super.key,
     required this.text,
     required this.onPressed,
-    this.backgroundColor,
-    this.textColor,
   });
 
   @override
@@ -21,20 +17,7 @@ class MyButton extends StatelessWidget {
       height: 50,
       child: ElevatedButton(
         onPressed: onPressed,
-        style: ElevatedButton.styleFrom(
-          backgroundColor: backgroundColor ?? const Color(0xFFFF6518),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-        ),
-        child: Text(
-          text,
-          style: TextStyle(
-            fontSize: 16,
-            color: textColor ?? Colors.white,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
+        child: Text(text),
       ),
     );
   }
