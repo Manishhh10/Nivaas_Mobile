9905931a98851755aefe09f0e0efffd995a632dc
Manishhh10 <manishbhatt2121714@gmail.com>
2026-01-03 00:21:47 +0545

    hive_table setup

---
+++ lib/core/constants/hive_table_constant.dart
@@ -0,0 +1,8 @@
+class HiveTableConstant {
+  HiveTableConstant._();
+
+  static const String dbName = "nivaas_db";
+
+  static const int userTypeId = 0;
+  static const String userTable = "user_table";
+}
