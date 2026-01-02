3da49e0257af5cc271ee9161e2943e81a6d02dfd
Manishhh10 <manishbhatt2121714@gmail.com>
2026-01-03 00:23:41 +0545

    hive_service completed

---
+++ lib/core/services/hive/hive_service.dart
@@ -0,0 +1,75 @@
+import 'package:hive_flutter/adapters.dart';
+import 'package:nivaas/core/constants/hive_table_constant.dart';
+import 'package:nivaas/features/auth/data/models/user_hive_model.dart';
+import 'package:path_provider/path_provider.dart';
+
+class HiveService {
+  Future<void> init() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = '${directory.path}/${HiveTableConstant.dbName}';
+    Hive.init(path);
+
+    _registerAdapters();
+    await _openBoxes();
+  }
+
+  void _registerAdapters() {
+    if (!Hive.isAdapterRegistered(0)) {
+      Hive.registerAdapter(UserHiveModelAdapter());
+    }
+  }
+
+  Future<void> _openBoxes() async {
+    await Hive.openBox<UserHiveModel>(HiveTableConstant.userTable);
+    await Hive.openBox('authBox'); // For storing login state
+  }
+
+  Box<UserHiveModel> get _userBox =>
+      Hive.box<UserHiveModel>(HiveTableConstant.userTable);
+
+  Box get _authBox => Hive.box('authBox');
+
+  // User CRUD Operations
+  Future<void> createUser(UserHiveModel user) async {
+    await _userBox.put(user.email, user);
+  }
+
+  List<UserHiveModel> getAllUsers() {
+    return _userBox.values.toList();
+  }
+
+  UserHiveModel? getUserByEmail(String email) {
+    return _userBox.get(email);
+  }
+
+  Future<void> updateUser(UserHiveModel user) async {
+    await _userBox.put(user.email, user);
+  }
+
+  Future<void> deleteUser(String email) async {
+    await _userBox.delete(email);
+  }
+
+  // Auth State Management
+  Future<void> saveLoginState(String email) async {
+    await _authBox.put('loggedInUser', email);
+    await _authBox.put('isLoggedIn', true);
+  }
+
+  String? getLoggedInUser() {
+    return _authBox.get('loggedInUser');
+  }
+
+  bool isLoggedIn() {
+    return _authBox.get('isLoggedIn', defaultValue: false) as bool;
+  }
+
+  Future<void> logout() async {
+    await _authBox.put('loggedInUser', null);
+    await _authBox.put('isLoggedIn', false);
+  }
+
+  Future<void> close() async {
+    await Hive.close();
+  }
+}
