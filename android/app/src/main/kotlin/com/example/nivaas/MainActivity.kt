dfd9f2c5464a3d24b2c8a1432adc03767075c105
Manishhh10 <manishbhatt2121714@gmail.com>
2026-03-01 13:36:49 +0545

    reorganize android kotlin entry points

--- android/app/src/main/kotlin/com/example/nivaas/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/nivaas/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.manish.nivaas
+package com.example.nivaas
 
 import io.flutter.embedding.android.FlutterActivity
 
---
+++ android/app/src/main/kotlin/com/manish/nivaas/nivaas/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.manish.nivaas.nivaas
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity : FlutterFragmentActivity()
